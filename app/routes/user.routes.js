62cecb9eb5e3afa68a80b9f5f0e42a1abe327007
j-engineers <ugboguj@yahoo.com>
2020-11-25 13:10:04 +0100

    Splitting the wocman website module

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -36,6 +36,29 @@ var uploadCert = multer({
     }
 });
 
+var storageProfile = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join('', 'app/', 'uploads/wocman/picture'))
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)+ path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+});
+var upload = multer({ 
+    storage: storageProfile, 
+    fileFilter : (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+            cb(null, true);
+
+        }else{
+            cb(null, false);
+            return cb(new Error('Only jpeg, jpg, png, gif file extensions are allowerd'));
+        }
+    }
+});
+
+
 module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
--- app/routes/website.routes.js
+++ app/routes/website.routes.js
@@ -48,69 +48,69 @@ module.exports = function(app) {
         next();
     });
 
-    // //website endpoints
-    // app.get(
-    //     Helpers.apiVersion7() + "get-location/:location", 
-    //     [], 
-    //     searchController.locationData
-    // );
+    //website endpoints
+    app.get(
+        Helpers.apiVersion7() + "get-location/:location", 
+        [], 
+        searchController.locationData
+    );
 
-    // app.get(
-    //     Helpers.apiVersion7() + "get-news-letters-subscribers", 
-    //     [], 
-    //     newsletterController.newsletter
-    // );
+    app.get(
+        Helpers.apiVersion7() + "get-news-letters-subscribers", 
+        [], 
+        newsletterController.newsletter
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7() + "subscribe-news-letters", 
-    //     [], 
-    //     addnewsletterController.subscribenewsletter
-    // );
+    app.post(
+        Helpers.apiVersion7() + "subscribe-news-letters", 
+        [], 
+        addnewsletterController.subscribenewsletter
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7() + "contact-us",
-    //     [], 
-    //     contactController.contactus
-    // );
+    app.post(
+        Helpers.apiVersion7() + "contact-us",
+        [], 
+        contactController.contactus
+    );
 
 
-    // app.get(
-    //     Helpers.apiVersion7()+"wocman-signup-verification/:link",
-    //     [],
-    //     emailverifyController.checkVerifyEmailLinkWocman
-    // );
+    app.get(
+        Helpers.apiVersion7()+"wocman-signup-verification/:link",
+        [],
+        emailverifyController.checkVerifyEmailLinkWocman
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7()+"password-reset-wocman",
-    //     [],
-    //     sendchangepasswordController.wocmanResetPassword
-    // );
+    app.post(
+        Helpers.apiVersion7()+"password-reset-wocman",
+        [],
+        sendchangepasswordController.wocmanResetPassword
+    );
 
-    // app.get(
-    //     Helpers.apiVersion7()+"wocman-password-reset/:link",
-    //     [],
-    //     confirmpasswordresetController.wocmanResetPasswordConfirm
-    // );
+    app.get(
+        Helpers.apiVersion7()+"wocman-password-reset/:link",
+        [],
+        confirmpasswordresetController.wocmanResetPasswordConfirm
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7()+"wocman-password-reset",
-    //     [],
-    //     resetpasswordController.wocmanStartResetPassword
-    // );
+    app.post(
+        Helpers.apiVersion7()+"wocman-password-reset",
+        [],
+        resetpasswordController.wocmanStartResetPassword
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7()+"auth/wocman-signin",
-    //     [
-    //         verifySignUp.checkRolesExisted
-    //     ],
-    //     signinController.signInWocman
-    // );
+    app.post(
+        Helpers.apiVersion7()+"auth/wocman-signin",
+        [
+            verifySignUp.checkRolesExisted
+        ],
+        signinController.signInWocman
+    );
 
-    // app.post(
-    //     Helpers.apiVersion7() + "auth/wocman-signup",
-    //     [
+    app.post(
+        Helpers.apiVersion7() + "auth/wocman-signup",
+        [
             
-    //     ],
-    //     signupController.signUpWocman
-    // );
+        ],
+        signupController.signUpWocman
+    );
 };
