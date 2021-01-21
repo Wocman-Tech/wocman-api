d6bd3a30cabd411510fd5495fa99d6a50fd41eb7
j-engineers <ugboguj@yahoo.com>
2021-01-21 02:16:21 +0100

    Updates

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -2,34 +2,36 @@ const { workstation }  = require("./env.config");
 
 if (workstation.toLowerCase() === "web") {
     module.exports = {
-        secret: "wocman-technology-api-justice-auth-0-security",
-        resolve: "https://wocman-node-api-8080.herokuapp.com",
+        secret: "",
+        resolve: "",
         port: '/',
         version: "/api/v1/",
         coreRootFolder: "",
         coreImageFloder: "app/uploads/",
         split: "/XX98XX",
-        email: "ugboguj@yahoo.com",
+        email: "",
         message_server: "Yahoo",
-        password: "rrilmtbqmsduewpy",
+        password: "",
         name: "Wocman Technology",
-        website: "https://wocman.netlify.app"
+        website: ""
+
     }
 }
 
 if(workstation.toLowerCase() === "localhost"){
     module.exports = {
-        secret: "wocman-technology-api-justice-auth-0-security",
-        resolve: "http://localhost",
-        port: ':8080/',
+        secret: "",
+        resolve: "",
+        port: '',
         version: "/api/v1/",
         coreRootFolder: "",
         coreImageFloder: "app/uploads/",
         split: "/XX98XX",
-        email: "ugboguj@yahoo.com",
+        email: "",
         message_server: "Yahoo",
-        password: "rrilmtbqmsduewpy",
+        password: "",
         name: "Wocman Technology",
-        website: "https://wocman.netlify.app"
+        website: ""
+
     }
 }
