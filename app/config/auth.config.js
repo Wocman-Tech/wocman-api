79f1bce2a442074bb34bacbbafdade6d3ef854fa
j-engineers <ugboguj@yahoo.com>
2021-04-03 10:00:16 +0100

    Updates on notification Push

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -2,43 +2,42 @@ const { workstation }  = require("./env.config");
 
 
 //auth.config.js
-
 if (workstation.toLowerCase() === "web") {
     module.exports = {
-        secret: "wocman-technology-api-justice-auth-0-security",
-        resolve: "https://wocman-node-api-8080.herokuapp.com",
+        secret: "",
+        resolve: "",
         port: '/',
         version: "/api/v1/",
         coreRootFolder: "",
         coreImageFloder: "app/uploads/",
         split: "/XX98XX",
-        email: "ugboguj@yahoo.com",
+        email: "",
         message_server: "Yahoo",
-        password: "rrilmtbqmsduewpy",
+        password: "",
         name: "Wocman Technology",
-        website: "https://wocman.netlify.app",
-        otpId: "8C5YFUT5NWJo8TD7tVQ20o4QF",
-        googleAppClientID: "832018209601-v1h3d5qgu10p3m4pv6l1jvgq9gcrl9v1.apps.googleusercontent.com",
-        googleAppClientSecret: "o1pyycovPehN0loXowHRh0aR"
+        website: "",
+        otpId: "",
+        googleAppClientID: "",
+        googleAppClientSecret: ""
     }
 }
 
 if(workstation.toLowerCase() === "localhost"){
     module.exports = {
-        secret: "wocman-technology-api-justice-auth-0-security",
-        resolve: "http://localhost",
-        port: ':8080/',
+        secret: "",
+        resolve: "",
+        port: '',
         version: "/api/v1/",
         coreRootFolder: "",
         coreImageFloder: "app/uploads/",
         split: "/XX98XX",
-        email: "ugboguj@yahoo.com",
+        email: "",
         message_server: "Yahoo",
-        password: "rrilmtbqmsduewpy",
+        password: "",
         name: "Wocman Technology",
-        website: "https://wocman.netlify.app",
-        otpId: "8C5YFUT5NWJo8TD7tVQ20o4QF",
-        googleAppClientID: "832018209601-v1h3d5qgu10p3m4pv6l1jvgq9gcrl9v1.apps.googleusercontent.com",
-        googleAppClientSecret: "o1pyycovPehN0loXowHRh0aR"
+        website: "",
+        otpId: "",
+        googleAppClientID: "",
+        googleAppClientSecret: ""
     }
 }
