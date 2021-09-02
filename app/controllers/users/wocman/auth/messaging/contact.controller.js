554d9b0dac4e63d8cbf3943b71fd83832f86a551
j-engineers <ugboguj@yahoo.com>
2021-09-02 02:51:13 +0100

    Updates

--- app/controllers/users/wocman/auth/messaging/contact.controller.js
+++ app/controllers/users/wocman/auth/messaging/contact.controller.js
@@ -110,7 +110,9 @@ exports.wocmanChatContact = (req, res, next) => {
                 cartItem3.customerEmail = cust.email
                 cartItem3.customerPhone = cust.phone
                 cartItem3.customerUsername = cust.username,
+                cartItem3.image = project.image,
                 cartItem3.customerId = project.customerid
+
                
                 // Simple push will work in this loop, you don't need to return anything
                 if (parseInt(project.wocmanaccept) > 1 && parseInt(project.wocmanaccept) < 5) {
