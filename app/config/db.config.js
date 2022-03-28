7205fed9c38d45777089bec0f28ffa03b4c26b78
Oluwafemi  Babayemi <femi.gabs@gmail.com>
2022-03-28 13:57:55 +0100

    update config file

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,38 +1,3 @@
-// const { workstation }  = require("./env.config");
-
-// //db.config.js
-// if (workstation.toLowerCase() == "web") {
-//     module.exports = {
-//         HOST: "us-cdbr-east-02.cleardb.com",
-//         USER: "bdddac9b8448ab",
-//         PASSWORD: "f851bc55",
-//         DB: "heroku_a9bd7ce27cad867",
-//         dialect: "mysql",
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             acquire: 30000,
-//             idle: 10000
-//         }
-//     }
-// }
-
-// if(workstation.toLowerCase() == "localhost"){
-//     module.exports = {
-//         HOST: "us-cdbr-east-02.cleardb.com",
-//         USER: "bdddac9b8448ab",
-//         PASSWORD: "f851bc55",
-//         DB: "heroku_a9bd7ce27cad867",
-//         dialect: "mysql",
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             acquire: 30000,
-//             idle: 10000
-//         }
-//     }
-// }
-
 require('dotenv').config();
 
 module.exports = {
