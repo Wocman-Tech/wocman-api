a124f0db3a4bcd2cd65b2bfe8ce2d70ed1f5c4c5
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2025-05-27 09:20:56 +0100

    Commented out endpoints with missging controllers

--- app/routes/adminwebsite.routes.js
+++ app/routes/adminwebsite.routes.js
@@ -1,5 +1,11 @@
 const { authJwt } = require("../middleware");
-const { isIdVerify, verifyAdminUser, rootAdmin, rootAction, initiateAdmin } = require("../middleware/admin");
+const {
+  isIdVerify,
+  verifyAdminUser,
+  rootAdmin,
+  rootAction,
+  initiateAdmin,
+} = require("../middleware/admin");
 
 const newsletterController = require("../controllers/users/admin/auth/website/newslettersubscribers.controller");
 const contactController = require("../controllers/users/admin/auth/website/contactus.controller");
@@ -9,73 +15,80 @@ const Helpers = require("../helpers/helper.js");
 const path = require("path");
 const multer = require("multer");
 
-const  { verifyAdminSignUp } = require("../middleware/website/user/admin");
-
+const { verifyAdminSignUp } = require("../middleware/website/user/admin");
 
 var storage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-        cb(null, '');
-    }
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
 });
-const uploadJob = multer({storage: storage}).array('avatar')
+const uploadJob = multer({ storage: storage }).array("avatar");
 
-
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
-
-    //auth
-    app.get(
-        Helpers.apiVersion7() + "admin/website/get/all/newsletterssubscribers", 
-        [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction], 
-        newsletterController.AllNewsletter
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
     );
+    next();
+  });
 
-    app.get(
-        Helpers.apiVersion7() + "admin/website/get/newsletterssubscriber/:id", 
-        [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction], 
-        newsletterController.oneNewsletter
-    );
+  //auth
+  app.get(
+    Helpers.apiVersion7() + "admin/website/get/all/newsletterssubscribers",
+    [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction],
+    newsletterController.AllNewsletter
+  );
 
-    app.get(
-        Helpers.apiVersion7() + "admin/website/delete/newsletterssubscriber/:id", 
-        [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction], 
-        newsletterController.deleteNewsletter
-    );
+  app.get(
+    Helpers.apiVersion7() + "admin/website/get/newsletterssubscriber/:id",
+    [
+      authJwt.verifyToken,
+      authJwt.isAdmin,
+      isIdVerify.isIdVerify,
+      rootAction.isRootAction,
+    ],
+    newsletterController.oneNewsletter
+  );
 
-    app.get(
-        Helpers.apiVersion7() + "admin/website/get/all/contactus", 
-        [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction], 
-        contactController.allContacts
-    );
+  app.get(
+    Helpers.apiVersion7() + "admin/website/delete/newsletterssubscriber/:id",
+    [
+      authJwt.verifyToken,
+      authJwt.isAdmin,
+      isIdVerify.isIdVerify,
+      rootAction.isRootAction,
+    ],
+    newsletterController.deleteNewsletter
+  );
 
-    app.get(
-        Helpers.apiVersion7() + "admin/website/get/conntactus/:id", 
-        [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction], 
-        contactController.oneContact
-    );
+  // app.get(
+  //     Helpers.apiVersion7() + "admin/website/get/all/contactus",
+  //     [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction],
+  //     contactController.allContacts
+  // );
 
-    app.get(
-        Helpers.apiVersion7() + "admin/website/delete/contactus/:id", 
-        [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction], 
-        contactController.deleteContact
-    );
+  // app.get(
+  //     Helpers.apiVersion7() + "admin/website/get/conntactus/:id",
+  //     [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction],
+  //     contactController.oneContact
+  // );
 
-    app.post(
-        Helpers.apiVersion7() + "admin/website/replay/chat", 
-        [authJwt.verifyToken, authJwt.isAdmin,  rootAction.isRootAction, uploadJob], 
-        contactController.replayContact
-    );
+  // app.get(
+  //     Helpers.apiVersion7() + "admin/website/delete/contactus/:id",
+  //     [authJwt.verifyToken, authJwt.isAdmin, isIdVerify.isIdVerify, rootAction.isRootAction],
+  //     contactController.deleteContact
+  // );
 
-    app.post(
-        Helpers.apiVersion7() + "admin/website/send/newsletter", 
-        [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction, uploadJob], 
-        newsletterController.sendNewsletter
-    );
+  app.post(
+    Helpers.apiVersion7() + "admin/website/replay/chat",
+    [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction, uploadJob],
+    contactController.replayContact
+  );
 
+  app.post(
+    Helpers.apiVersion7() + "admin/website/send/newsletter",
+    [authJwt.verifyToken, authJwt.isAdmin, rootAction.isRootAction, uploadJob],
+    newsletterController.sendNewsletter
+  );
 };
--- app/routes/wocmanproject.routes.js
+++ app/routes/wocmanproject.routes.js
@@ -9,96 +9,101 @@ const startController = require("../controllers/users/wocman/auth/workstation/st
 const stopController = require("../controllers/users/wocman/auth/workstation/stop.controller");
 const chatController = require("../controllers/users/wocman/auth/workstation/chat.controller");
 
-
 const Helpers = require("../helpers/helper.js");
 
 const path = require("path");
 const multer = require("multer");
-const { getApprovedProjects, acceptJob, completeJob } = require("../controllers/users/wocman/auth/dashboard/jobs.controller");
-const { getSingleProject } = require("../controllers/admin/dashboard.controller");
+const {
+  getApprovedProjects,
+  acceptJob,
+  completeJob,
+} = require("../controllers/users/wocman/auth/dashboard/jobs.controller");
+const {
+  getSingleProject,
+} = require("../controllers/admin/dashboard.controller");
 
 var storage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-        cb(null, '');
-    }
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
 });
-const uploadChat = multer({storage: storage}).array('avatar')
+const uploadChat = multer({ storage: storage }).array("avatar");
 
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
-    //project routes
+  //project routes
 
-    app.get(
-        Helpers.apiVersion7() + "wocman/project/approved", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        getApprovedProjects
-    );
+  app.get(
+    Helpers.apiVersion7() + "wocman/project/approved",
+    [authJwt.verifyToken, authJwt.isWocman],
+    getApprovedProjects
+  );
 
-    app.put(
-        Helpers.apiVersion7() + "wocman/project/accept/:id", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        acceptJob
-    );
+  app.put(
+    Helpers.apiVersion7() + "wocman/project/accept/:id",
+    [authJwt.verifyToken, authJwt.isWocman],
+    acceptJob
+  );
 
-    app.put(
-        Helpers.apiVersion7() + "wocman/project/complete/:id", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        completeJob
-    );
+  app.put(
+    Helpers.apiVersion7() + "wocman/project/complete/:id",
+    [authJwt.verifyToken, authJwt.isWocman],
+    completeJob
+  );
 
-    app.get(
-        Helpers.apiVersion7() + "wocman/project/:id", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        getSingleProject
-    );
+  app.get(
+    Helpers.apiVersion7() + "wocman/project/:id",
+    [authJwt.verifyToken, authJwt.isWocman],
+    getSingleProject
+  );
 
-    app.post(
-        Helpers.apiVersion7() + "wocman-project-customer", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        customerController.wocmanProjectCustomer
-    );
+  app.post(
+    Helpers.apiVersion7() + "wocman-project-customer",
+    [authJwt.verifyToken, authJwt.isWocman],
+    customerController.wocmanProjectCustomer
+  );
 
-    app.post(
-        Helpers.apiVersion7() + "wocman-project-customer-chat-log", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        chatController.chatLog
-    );
-    app.post(
-        Helpers.apiVersion7() + "wocman-project-customer-chat-save", 
-        [authJwt.verifyToken, authJwt.isWocman, uploadChat], 
-        chatController.chatSave
-    );
+  // app.post(
+  //     Helpers.apiVersion7() + "wocman-project-customer-chat-log",
+  //     [authJwt.verifyToken, authJwt.isWocman],
+  //     chatController.chatLog
+  // );
+  app.post(
+    Helpers.apiVersion7() + "wocman-project-customer-chat-save",
+    [authJwt.verifyToken, authJwt.isWocman, uploadChat],
+    chatController.chatSave
+  );
 
-    app.post(
-        Helpers.apiVersion7() + "wocman-accept-project", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        acceptController.wocmanAcceptProject
-    );
-    app.post(
-        Helpers.apiVersion7() + "wocman-reject-project", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        rejectController.wocmanRejectProject
-    );
-    app.post(
-        Helpers.apiVersion7() + "wocman-start-project", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        startController.wocmanStartProject
-    );
-    app.post(
-        Helpers.apiVersion7() + "wocman-stop-project", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        stopController.wocmanStopProject
-    );
-    app.post(
-        Helpers.apiVersion7() + "wocman-complete-project", 
-        [authJwt.verifyToken, authJwt.isWocman], 
-        completeController.wocmanCompleteProject
-    );
+  app.post(
+    Helpers.apiVersion7() + "wocman-accept-project",
+    [authJwt.verifyToken, authJwt.isWocman],
+    acceptController.wocmanAcceptProject
+  );
+  app.post(
+    Helpers.apiVersion7() + "wocman-reject-project",
+    [authJwt.verifyToken, authJwt.isWocman],
+    rejectController.wocmanRejectProject
+  );
+  app.post(
+    Helpers.apiVersion7() + "wocman-start-project",
+    [authJwt.verifyToken, authJwt.isWocman],
+    startController.wocmanStartProject
+  );
+  app.post(
+    Helpers.apiVersion7() + "wocman-stop-project",
+    [authJwt.verifyToken, authJwt.isWocman],
+    stopController.wocmanStopProject
+  );
+  app.post(
+    Helpers.apiVersion7() + "wocman-complete-project",
+    [authJwt.verifyToken, authJwt.isWocman],
+    completeController.wocmanCompleteProject
+  );
 };
